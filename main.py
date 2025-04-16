557f6cd059ae50a702eec1cd0beeb5f2ca0d8172
aaran <aranovichabigail698@gmail.com>
2025-04-16 08:15:41 +0300

    cluster analysis complete errorless

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from odf.opendocument import load
 from odf.table import Table, TableRow, TableCell
 import pandas as pd
 import numpy as np
+def isCellNa(series):
+    return pd.isna(series[0])
 
 def isFileValid(file_path):
     if os.path.exists(file_path):
@@ -35,7 +37,6 @@ def getTablesFromWord(file_path):
     doc = load(file_path)
     return doc.getElementsByType(Table)
 
-
 def getSpotCompositionData(spot_word_table):
     cluster_composition_columns = [ 'Ca2+', 'P3-', 'Zn2+', 'Mg2+', 'Ca&P',
                        'Non-Cap(only Calcium)', 'Zn', 'Ca:Mg =1']
@@ -45,9 +46,8 @@ def getSpotCompositionData(spot_word_table):
         element_weight = float(str(row.getElementsByType(TableCell)[4]))
         if element_weight > 0.4:
             element_num = int(str(row.getElementsByType(TableCell)[1]))
-            print(element_num)
             if element_num == 20:
-                spot_comp['Ca2+'] = [element_weight]
+                spot_comp['Ca2+'] = [element_weight]## need to check how to input NO
             if element_num == 15:
                 spot_comp['P3-'] = [element_weight]
             if element_num == 30:
@@ -55,22 +55,25 @@ def getSpotCompositionData(spot_word_table):
             if element_num == 11:
                 spot_comp['Mg2+'] = [element_weight]
 
-    if not spot_comp['Zn2+'].isna():
+    if not isCellNa(spot_comp['Zn2+']):
         spot_comp['Zn'] = ['Yes']
     else:
         spot_comp['Zn'] = ['No']
 
-    if not spot_comp['Ca2+'].empty:
+    if not isCellNa(spot_comp['P3-']):
 
-        if not spot_comp['P3-'].empty:
+        if not isCellNa(spot_comp['Ca2+']):
             spot_comp['Ca&P'] = ['Yes']
             spot_comp['Non-Cap(only Calcium)'] = ['No']
         else:
             spot_comp['Ca&P'] = ['No']
-            spot_comp['Non-Cap(only Calcium)'] = ['Yes']
+            spot_comp['Non-Cap(only Calcium)'] = ['No']
     else:
         spot_comp['Ca&P'] = ['No']
-        spot_comp['Non-Cap(only Calcium)'] = ['Yes']
+        if not isCellNa(spot_comp['Ca2+']):
+          spot_comp['Non-Cap(only Calcium)'] = ['Yes']
+
+
     cal_to_mg_ratio = spot_comp['Ca2+'].iloc[0] / spot_comp['Mg2+'].iloc[0]
 
     if 1.1 >= cal_to_mg_ratio >= 0.9:
@@ -82,27 +85,19 @@ def getSpotCompositionData(spot_word_table):
 
 def getSpotIdentification():
     return
+
 def getAnalysedClusterData(file_path):
 
     spot_tables = getTablesFromWord(file_path)
     analysed_cluster_data = []
-    for spot_table_index, spot_table in enumerate(spot_tables[-3:]):
+    for spot_table_index, spot_table in enumerate(spot_tables):# gets last three tables
         print("Table:", spot_table_index + 1)
         # analysed_cluster_data.append(table)
         print(getSpotCompositionData(spot_table))
     return analysed_cluster_data
 
-        # for row_index, row in enumerate(table.getElementsByType(TableRow)):
-        #     row_data = []
-        #     for cell_index, cell in enumerate(row.getElementsByType(TableCell)[1:]):
-        #         cell_content = ""
-        #         for child in cell.childNodes:
-        #             cell_content += str(child)
-        #
 
-
-
-def getClusterData(file_path = 'word format/sample 11.odt'):
+def getClusterData(file_path = 'Word Uneditted/sample 11.odt'):
     cluster_data = []
     if isFileValid(file_path):
         cluster_data = getAnalysedClusterData(file_path)
